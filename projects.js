bbae9a656e9456f971ec92580343749e7333ab57
kathy wu <kaaathywu@gmail.com>
2021-12-31 14:12:10 -0500

    add site

---
+++ projects.js
@@ -0,0 +1,130 @@
+export default
+[ {
+  shown:false,
+  date:"Due 9/16",
+  project:"Exercise A",
+  name:"Computational Histories Potluck",
+  content:`
+  Decide on a short research topic to share with the class next week, relating to the history of computers, society, from a lens of resistance— decolonial, anti-surveillance, intersectionally feminist, all of the above...
+  <br><br>
+  <img src="imgs/abtec.jpg" class="imageCaption"><br><span class="imageCaption">ABTEC Island, Skawennati</span>
+  <br><br>
+  Bring at least 1 historical reference—for instance, the invention of the jacquard loom and its effect on labor. How women in World Wars encoded secret knit messages into sweaters. Or QuiPu as an indigineous mode of data storage.
+  <br><br>
+  Share also 2-3 examples of contemporary computational artworks which aligns with your theme.This can be projects, writings, initiatives from artists, advocates, policy makers working recently. For instance, anti-surveillance workshops or browser extensions, or sex worker demands to sell sex on the internet.
+  <br><br>
+
+  Post these to the working <a href="https://docs.google.com/document/d/1_sBis5J5TqXkC9tcPxYOXH9CH7-e0Je2M4vISRcPCsE/edit?usp=sharing" target="_blank">google doc</a>. For each reference, add it to the shared google doc with an image, link, and a short description. Be prepared to informally present with the class.
+  `
+  },
+  {
+    shown:false,
+    date:"Due 9/23",
+    project:"Exercise B",
+    name:"Gliiitch",
+    content:`Legacy Russell describes the glitch as “an error, a mistake, a failure to function,” celebrated as “a vehicle of refusal, a strategy of non-performance.” How might these principles and ideas enter our work this semester?
+<br><br>
+<img src="imgs/rosamenkman.png" class="imageCaption"><br><span class="imageCaption">Detail of glitch art by Rosa Menkman, 2011</span><br><br>
+
+In one week, apply these principles of glitching to a piece of text. Create at least 5 images which incorporate the “glitch” and language.
+<br><br>
+<img src="imgs/fc.png" class="imageCaption"><br><span class="imageCaption">Refraction, by Francesca Capone</span><br><br>
+To share your project, create a website which hosts your response as embedded images, and include a short description (100+) of your process and intent.
+<br><br>Since this is a one week project, think of it more like an exercise and experimentation than a fully-fleshed out project.
+Post your glitches in the Discord #projects channel.
+
+    `
+    },
+
+    {
+      shown:false,
+      date:"In Class",
+      project:"Exercise C",
+      name:"Transcription",
+      content:`
+      Transcribe 250-500 words of any text. It must be done by you, manually. This project is less about form and more about experience + process.
+      <br>
+      <br>
+      <img src="imgs/Bruno-Leyval.jpg" class="imageCaption"><br><span class="imageCaption">Automatic Writing, Bruno Leyval</span>
+      <br><br>
+      Consider your source text:
+      <li>Is it already presented as digital, parsable text?
+      <li>Is it archived as a printed or written object (or otherwise not-presently machine readable)?
+      <li>How spatial is it? (e.g. a set of unrelated receipts on the table, a set of street names)
+      <br>
+      <br>
+
+      In a few sentences in the Discord, articulate how transcription feels as a method of transformation. Note the bodily and mental experience of translation. Note the decisions you make when you are translating it. In which ways does it feel neutral, or opinionated?
+    `
+    },
+{
+		shown:false,
+    date:"Due 10/14",
+		project:"Project 1",
+    name:"Dice in the Browser",
+		content:`
+    Create a text-based browser project which uses variability and randomness in some way. This project will build on our in-class knowledge of HTML, CSS, and javascript.
+    <br><br>
+    <img src="imgs/troika.png" class="imageCaption"><br><span class="imageCaption">Troika, Calculating the Universe, 2014</span><br><br>
+    Themes from class which might inspire your project: oracles, divination, weather machines, horoscopes, things which tell the future.
+    <br>
+    <li> Must, at minimum, use randomness in some way
+    <li> Must demonstrate a basic knowledge of HTML/CSS and calling an array by index
+    <li> You can use an existing text (i.e. a poem, piece of news, etc) or you can write your own.
+    <br>
+    <br>
+    <img src="imgs/ants.jpg" class="imageCaption"><br><span class="imageCaption">Benjamin Patterson, Ants, 1960-1962</span>
+    <br><br>
+    To consider: How do computational ingredients affect the nature of the text? How does the literal and metaphorical concepts of randomness, repetition, score transform the text’s meaning?
+    `
+	},
+	{
+		shown:false,
+    date:"Due 11/04",
+		project:"Project 2",
+    name:"Embodied Voice(s)",
+		content:`
+    Create a project which brings together two or more authors (one of them can be you). This project might touch on themes of hypertext, collective writing, or authorship and found text as writing. It might touch on feedback loops and crowdsourcing. Perhaps writing becomes something of a collage. This project:
+    <br>
+    <br>
+    <li>Must draw a relationship between two or more pieces of text by different authors (one of them can be you)
+    <li>Must be performed for the class over zoom (or you can share a recording of the performance)
+    <li>Can use any medium of language expression: text, audio based, etc. Will you perform it? Will you use a voice interface other than yourself? Will it be silent?
+    <br>
+    <br>
+    <img src="imgs/skin.png" class="imageCaption"><br><span class="imageCaption">Skinonskinonskin, Auriea Harvey and Michaël Samyn, 1999</span><br><br>
+    <b>To consider:</b> How does the embodiment of the text shift the relationship between the original text(s)? Will you reveal difference between all/both voices, or channel through a disambiguating singular body?
+    `
+
+	},
+
+	{
+		shown:false,
+    date:"Due 12/02",
+		project:"Project 3",
+    name:"New Data Sets",
+		content:`Building off themes and questions of legibility and classification in this course, your final project is to either:
+    <li> define and publish a new dataset
+    <li> publish an existing dataset in a new way
+    <br><br>
+    <img src="imgs/dk.png" class="imageCaption"><br><span class="imageCaption">Dina Kelberman, I'm Google</span><br><br>
+
+    Projects that might support your thinking along this are:
+    <br><a href="https://enron.email/about"target="_blank">Sam Lavigne & Tega Brain, The Good Life</a>
+    <br><a href="https://docs.google.com/spreadsheets/d/14xj-_MAo1qCPAlVPWi5bI7tLEZJKJ-s8yxnHyedQce8/edit#gid=0"target="_blank">Google Street View Birding Life List</a>
+    <br><a href="https://a-counting.us/"target="_blank">Ekene Ijeoma, A Counting</a>
+    <br><a href="https://binarycalculationsareinadequate.org/"target="_blank">Stephanie Dinkins, Binary Calculations Are Inadequate</a>
+    <br><a href="https://dinakelberman.tumblr.com/"target="_blank">Dina Kelberman, I’m Google</a>
+    <br><a href="https://www.jamescohan.com/exhibitions/byron-kim3"target="_blank">Bryon Kim, Sunday Paintings</a>
+    <br><br>
+    <img src="imgs/bk.jpg" class="imageCaption"><br><span class="imageCaption">Byron Kim, Sunday Paintings</span><br><br>
+    For the first week, come in with 1-2 ideas of project directions for working with a data set. You might use the same dataset for both, or consider two different sets.
+    <br>e.g. I will collect every cloud I see on my walk.
+    <br>e.g. I will collect every comment made to me about X
+    <br>e.g. I will work with City of Providence data about Y
+    <br><br>
+    Focus on the process of data collection in your reflections. You should bring 40 total items to class. If they are something intangible, like hyperlinks, print out a tangible representation of it, such as a website.
+    `
+	},
+
+]
